5d23748c3bb106a4dfcea43474d4388362d7dc9b
Meowybot <jorgedidier149@gmail.com>
2026-02-07 16:51:10 -0600

    Update lang.lua

--- lang.lua
+++ lang.lua
@@ -10,3 +10,15 @@ meowy.reservedstrings = {
 ["return"] = true,
 ["break"] = true
 }
+
+function meowy.loadfile(filename, isLove)
+  print("loading file")
+  local itfunc
+  if isLove then
+    itfunc = love.filesystem.lines
+  else
+    itfunc = io.lines
+  end
+  for line in itfunc(filename) do
+  end
+end
