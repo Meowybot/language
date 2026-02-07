1626cfe226810548bbc7a5f0edf16a80bcd21273
Meowybot <jorgedidier149@gmail.com>
2026-02-07 15:30:49 -0600

    Create lang.lua

---
+++ lang.lua
@@ -0,0 +1,12 @@
+local meowy = {}
+meowy.loadedlines = {}
+meowy.reservedstrings = {
+["if"] = true,
+["?"] = true,
+["define"] = true,
+["while"] = true,
+["true"] = true,
+["false"] = true,
+["return"] = true,
+["break"] = true
+}
