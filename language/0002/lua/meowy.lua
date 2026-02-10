1f6bc416f82b20c735604a4c7c98c7f430b85fd6
Meowybot <jorgedidier149@gmail.com>
2026-02-10 17:17:50 -0600

    add toluafunc function

--- language/0002/lua/meowy.lua
+++ language/0002/lua/meowy.lua
@@ -1,5 +1,7 @@
 local meowy
 
+meowy.uf = {}
+
 meowy.reserved = {}
 
 function meowy.reserved['define'](line, d)
@@ -50,7 +52,73 @@ function meowy.loadFile(filename, isLove)
 end
 
 function meowy.toLuaFunc(line, d)
-  --please help
+  local luas = ""
+  if not d.onlyargs then
+    if meowy.uf[line[1]] then
+      luas = luas .. "meowy.uf['" .. line[1] .. "']("
+    elseif meowy.specialFuncs[line[1]] then
+      luas = luas .. "meowy.specialFuncs['" .. line[1] .. "']("
+    else
+      luas = luas .. "meowy.funcs['" .. line[1] .. "']("
+    end
+  end
+  local isNested = false
+  local NestStart = 0
+  local NestEnd = 0
+  local NestAmount = 0
+  for i = 2, #line do
+    if not isNested then
+      if string.sub(line[i], 1, 1) == "(" then
+        isNested = true
+        NestStart = i
+        NestAmount = 1
+        if string.sub(line[i], -1, -1) == ")" then
+          NestEnd = i
+          NestAmount = 0
+          break
+        end
+      else
+        --make the code do silly stuff
+      end
+    else
+      if string.sub(line[i], -1, -1) == ")" then
+        NestAmount=NestAmount - 1
+        if NestAmount == 0 then
+          NestEnd = i
+          break
+        end
+      elseif string.sub(line[i], 1, 1 == "(" and NestAmount > 0 then
+        NestAmount = NestAmount + 1
+      end
+    end
+  end
+  if isNested then
+    local newlinr = {}
+    if NestStart == NestEnd then
+      table.insert(newlinr, string.sub(line[NestStart], 2, -2))
+    elseif NestEnd - Neststart == 1 then
+      newlinr[1] = string.sub(line[NestStart], 2, -1)
+      newlinr[2] = string.sub(line[NestEnd], 1, -2)
+    else
+      newlinr[1] = string.sub(line[NestStart], 2, -1)
+      for i = NestStart + 1, NestEnd - 1 do
+        table.insert(newlinr, line[i])
+      end
+      table.insert(newlinr, string.sub(line[NestEnd], 1, -2))
+    end
+    luas = luas .. meowy.toLuaFunc(newlinr, {})
+    if NestEnd ~= #line then
+      luas = luas .. ", "
+      local newnewlinr = {}
+      newnewlinr[1] = line[1]
+      for i = NestEnd + 1, #line do
+        table.insert(newnewlinr, line[i])
+      end
+      luas = luas .. meowy.toLuaFunc(newnewlinr, {onlyargs = true})
+    end
+  end
+  luas = luas .. ")" .. "\n"
+  return luas
 end
 
 function meowy.toLua(t)
