60ea8aba2b2702c8f2de7925cb1cb0f2d8871fd6
Meowybot <jorgedidier149@gmail.com>
2026-02-10 12:58:32 -0600

    Update meowy.lua

--- language/0002/lua/meowy.lua
+++ language/0002/lua/meowy.lua
@@ -2,7 +2,8 @@ local meowy
 
 meowy.reserved = {}
 
-function meowy.reserved['define'](line, luas, d)
+function meowy.reserved['define'](line, d)
+  local luas = ""
   luas = luas .. line[2] .. " = function("
   if #line > 3 then
     for i = 3, #line - 1 do
@@ -13,10 +14,13 @@ function meowy.reserved['define'](line, luas, d)
     end
   end
   luas = luas .. ")" .. "\n"
+  return luas
 end
 
-function meowy.reserved['}'](line, luas, d)
+function meowy.reserved['}'](line, d)
+  local luas = ""
   luas = luas .. "end" .. "\n"
+  return luas
 end
 
 
@@ -28,4 +32,7 @@ meowy.specialFuncs = {}
 
 meowy.specialWords = {}
 
+
+function meowy.toLuaFunc(line, d)
+  --please help
 return meowy
