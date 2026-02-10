28807ea05ea0754c8277447a3a6235e682a71625
Meowybot <jorgedidier149@gmail.com>
2026-02-10 10:54:52 -0600

    add define keyword to 0.2

--- language/0002/lua/meowy.lua
+++ language/0002/lua/meowy.lua
@@ -2,6 +2,20 @@ local meowy
 
 meowy.reserved = {}
 
+function meowy.reserved['define'](line, luas, d)
+  luas = luas .. line[2] .. " = function("
+  if #line > 3 then
+    for i = 3, #line - 1 do
+      luas = luas .. line[i]
+      if i < #line - 1 then
+        luas = luas .. ", "
+      end
+    end
+  end
+  luas = luas .. ")" .. "\n"
+end
+
+
 meowy.funcs = {}
 
 
