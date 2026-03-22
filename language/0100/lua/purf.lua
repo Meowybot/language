4b489bf62123db935afc03cb08ce025f624cf3e4
Meowybot <jorgedidier149@gmail.com>
2026-03-22 13:33:14 -0600

    Update purf.lua

--- language/0100/lua/purf.lua
+++ language/0100/lua/purf.lua
@@ -1,5 +1,15 @@
 local purf = {}
 
+local function deletestartspace(str)
+  return (str:gsub("^%s+", ""))
+end
 
+local function splitVariable(input)
+    local name, value = input:match("(.-) (.*)")
+    return {(name or input), (value or "")}
+end
+
+function purf.bla()
+end
 
 return purf
