cc20451aa2bd790a46d62dbf3b7c436677cc853b
Meowybot <jorgedidier149@gmail.com>
2026-06-28 19:31:56 -0600

    Update mwn.lua

--- lang/mwn.lua
+++ lang/mwn.lua
@@ -1,2 +1,15 @@
 local mwn = {}
+
+mwn.datatypes = {
+  ["string"] = function(v)
+    return tostring(v)
+  end,
+  ["number"] = function(v)
+    return tonumber(v) or 0
+  end,
+  ["boolean"] = function(v)
+    return v ~= "false"
+  end
+}
+
 return mwn
